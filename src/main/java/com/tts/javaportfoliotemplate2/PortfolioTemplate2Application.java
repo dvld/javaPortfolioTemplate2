ab965a3249a507fc752d18272630e99887b061b8
dvld <nikoroberts620@gmail.com>
2019-06-18 00:15:25 -0500

    init commit

---
+++ src/main/java/com/tts/javaportfoliotemplate2/PortfolioTemplate2Application.java
@@ -0,0 +1,13 @@
+package com.tts.javaportfoliotemplate2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioTemplate2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioTemplate2Application.class, args);
+	}
+
+}
